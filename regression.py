5313af1c6755b6e9352d0216e4d5f15f249790e8
Mariya Kazachkova <mariyakazachkova@Mariyas-MacBook-Pro.local>
2017-05-03 20:46:09 -0400

    linear and ridge

--- regression.py
+++ regression.py
@@ -124,7 +124,7 @@ X_train2, X_test2, y_train2, y_test2 = train_test_split(exp, ics, test_size = 0.
 
 
 #feature selection 
-"""for SNP""""""
+"""for SNP"""
 selector = VarianceThreshold()
 selector.fit(X_train)
 all_var = selector.variances_
@@ -137,7 +137,7 @@ for i in range(0, len(all_var)):
 	var_d[i] = all_var[i]
 
 sorted_var_d = sorted(var_d.items(), key=operator.itemgetter(1), reverse=True)
-"""
+
 
 """for GENE"""
 selector2 = VarianceThreshold()
@@ -157,7 +157,7 @@ sorted_var_d2 = sorted(var_d2.items(), key=operator.itemgetter(1), reverse=True)
 
 
 """Do feature selection for snp. Not taking features with very low variance and very high variance"""
-"""
+
 ind = []
 for i in range(50,5500):
 	ind.append(sorted_var_d[i][0])
@@ -173,13 +173,13 @@ for i in range(0,len(ind)):
 	tempXX = X_test[0:79,ind[i]]
 	X_train_selected.append(tempX)
 	X_test_selected.append(tempXX)
-"""
+
 
 
 
 """Do feature selection for Gene. Not taking features with very low variance and very high variance"""
 ind2 = []
-for i in range(0,3000):
+for i in range(0,6000):
 	ind2.append(sorted_var_d2[i][0])
 
 X_train_selected2 = []
@@ -195,9 +195,19 @@ for i in range(0,len(ind2)):
 	X_test_selected2.append(tempXX2)
 
 
+"""Now we move on to doing pca for gene"""
+pca_gene = PCA(30).fit(X_train2)
+components_gene = pca_gene.components_
+print (pca_gene.components_).shape
+gene_result_train = np.dot(X_train2, components_gene.transpose())
+print gene_result_train.shape
+
+gene_result_test = np.dot(X_test2, components_gene.transpose())
+print gene_result_test.shape
 
 
-"""Now we move on to doing pca"""
+
+"""Now we move on to doing pca for SNP"""
 pca = PCA(30).fit(X_train)
 components = pca.components_
 print (pca.components_).shape
@@ -208,20 +218,24 @@ snp_result_test = np.dot(X_test, components.transpose())
 print snp_result_test.shape
 
 
+
+
+
 """
+
 #linear regression
 lin = linear_model.LinearRegression()
 lin_fit = lin.fit(snp_result_train, y_train)
 lin2 = linear_model.LinearRegression()
-lin2_fit = lin2.fit(X_train2, y_train2)
+lin2_fit = lin2.fit(gene_result_train, y_train2)
 
 #calculate R^2 wth test sets
-print lin_fit.score(snp_result_test, y_test) 
-print lin2_fit.score(X_test2, y_test2) 
+print "R squared value SNP: " + str(lin_fit.score(snp_result_test, y_test))
+print "R squared value GENE: " + str(lin2_fit.score(gene_result_test, y_test2)) 
 
 #calculate MSE
 lin_predict = lin_fit.predict(snp_result_test)
-lin2_predict = lin2_fit.predict(X_test2)
+lin2_predict = lin2_fit.predict(gene_result_test)
 print "mean square error of linear regression on SNP is " + str(mean_squared_error(y_test, lin_predict)) #y_true, y_predict
 print "mean square error of linear regression on gene expression is " + str(mean_squared_error(y_test2,lin2_predict))
 
@@ -233,16 +247,47 @@ print lins2
 print("Accuracy: %0.2f (+/- %0.2f)" % (lins.mean(), lins.std() * 2))
 print("Accuracy: %0.2f (+/- %0.2f)" % (lins2.mean(), lins2.std() * 2))
 
-
-
 """
 
 
 
 
 
+#ridge regression
+ridge1 = linear_model.Ridge()
+ridge1_fit = ridge1.fit(snp_result_train, y_train)
+ridge2 = linear_model.Ridge()
+ridge2_fit = ridge2.fit(gene_result_train, y_train2)
+
+#calculate R^2 with test sets
+print "R squared value SNP: " + str(ridge1.score(snp_result_test, y_test)) 
+print "R squared value GENE: " + str(ridge2.score(gene_result_test, y_test2)) 
+
+#calculate MSE
+ridge_predict = ridge1_fit.predict(snp_result_test)
+ridge2_predict = ridge2_fit.predict(gene_result_test)
 
 
+print 'length of ridge predict is ' + str(len(ridge_predict))
+print "mean square error of ridge regression on SNP is " + str(mean_squared_error(y_test, ridge_predict)) #y_true, y_predict
+print "mean square error of ridge regression on gene expression is " + str(mean_squared_error(y_test2,ridge2_predict))
+
+#cross-validation 
+r = cross_val_score(ridge1, snp, ics, cv=10) 
+r2 = cross_val_score(ridge2, exp, ics, cv = 10) 
+print r
+print r2
+print("Accuracy: %0.2f (+/- %0.2f)" % (r.mean(), r.std() * 2))
+print("Accuracy: %0.2f (+/- %0.2f)" % (r2.mean(), r2.std() * 2)) 
+
+
+
+
+
+
+
+
+"""
 
 X_train_selected2 = np.array(X_train_selected2)
 X_test_selected2 = np.array(X_test_selected2)
@@ -260,7 +305,7 @@ print lin_fit2.score(X_test_selected2, y_test2)
 #calculate MSE
 lin_predict2 = lin_fit2.predict(X_test_selected2)
 print "mean square error of linear regression (after feature selection) on gene is " + str(mean_squared_error(y_test2, lin_predict2)) #y_true, y_predict
-
+"""
 
 
 
